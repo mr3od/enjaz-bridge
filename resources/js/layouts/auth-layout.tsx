ba86640fdd9bdcabedeacd5596f7d764bec3742f
mr3od <abdulrahman1ye@gmail.com>
2026-03-02 02:39:00 +0300

    Update resources JS layout components

--- resources/js/layouts/auth-layout.tsx
+++ resources/js/layouts/auth-layout.tsx
@@ -1,4 +1,5 @@
-import AuthLayoutTemplate from '@/layouts/auth/auth-simple-layout';
+import AuthLayoutTemplate from '@/layouts/auth/auth-split-layout'; 
+
 
 export default function AuthLayout({
     children,
